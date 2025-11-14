a6a8e6d0e606bdc2cb1864f03310f525fed5e6f1
nikhilreddy1102 <nikhilkothapally1102@gmail.com>
2025-11-14 00:26:24 -0600

    fixed nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 // next.config.ts
 import type { NextConfig } from 'next';
 
-const repo = 'Portfolio' as const; // must match your GitHub repo name exactly (case-sensitive)
-
 const nextConfig: NextConfig = {
-  output: 'export',              // build static files for GitHub Pages
-  images: { unoptimized: true }, // Pages is static; disable Next/Image optimizer
-  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
-  trailingSlash: true,           // ensures /about -> /about/index.html
+  reactStrictMode: true,
 };
 
 export default nextConfig;
