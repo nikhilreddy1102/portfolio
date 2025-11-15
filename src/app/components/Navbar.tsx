959a27f8314d0c67948704399b73ac6117fde192
nikhilreddy1102 <kothapallynikhil2000@gmail.com>
2025-11-15 00:13:22 -0600

    created rag pipeline

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -81,7 +81,7 @@ export default function Navbar() {
             <Link href="/#projects" onClick={close} className="block rounded-md px-3 py-2 text-gray-200 hover:bg-white/10">Projects</Link>
             <Link href="/#contact" onClick={close} className="block rounded-md px-3 py-2 text-gray-200 hover:bg-white/10">Contact</Link>
             <Link
-              href="/NikhilReddy_Resume.pdf"
+              href="/K_Nikhil_Reddy_Resume.pdf"
               prefetch={false}
               target="_blank"
               rel="noopener"
